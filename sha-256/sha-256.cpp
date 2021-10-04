992ca5ca4ee4f9c827dc3bd80aa461d004ac0a34
UnTea <weqqu2014@yandex.ru>
2021-10-04 23:39:40 +0300

    beaty refactor

--- sha-256/sha-256.cpp
+++ sha-256/sha-256.cpp
@@ -5,6 +5,40 @@
 #include <iomanip>
 
 
+static constexpr std::array<uint32_t, 64> SHA256_KEYS {
+        0x428a2f98, 0x71374491, 0xb5c0fbcf, 0xe9b5dba5,
+        0x3956c25b, 0x59f111f1, 0x923f82a4, 0xab1c5ed5,
+        0xd807aa98, 0x12835b01, 0x243185be, 0x550c7dc3,
+        0x72be5d74, 0x80deb1fe, 0x9bdc06a7, 0xc19bf174,
+        0xe49b69c1, 0xefbe4786, 0x0fc19dc6, 0x240ca1cc,
+        0x2de92c6f, 0x4a7484aa, 0x5cb0a9dc, 0x76f988da,
+        0x983e5152, 0xa831c66d, 0xb00327c8, 0xbf597fc7,
+        0xc6e00bf3, 0xd5a79147, 0x06ca6351, 0x14292967,
+        0x27b70a85, 0x2e1b2138, 0x4d2c6dfc, 0x53380d13,
+        0x650a7354, 0x766a0abb, 0x81c2c92e, 0x92722c85,
+        0xa2bfe8a1, 0xa81a664b, 0xc24b8b70, 0xc76c51a3,
+        0xd192e819, 0xd6990624, 0xf40e3585, 0x106aa070,
+        0x19a4c116, 0x1e376c08, 0x2748774c, 0x34b0bcb5,
+        0x391c0cb3, 0x4ed8aa4a, 0x5b9cca4f, 0x682e6ff3,
+        0x748f82ee, 0x78a5636f, 0x84c87814, 0x8cc70208,
+        0x90befffa, 0xa4506ceb, 0xbef9a3f7, 0xc67178f2
+};
+
+SHA256::SHA256()
+: m_block_length(0)
+, m_bit_length(0)
+, m_state {
+    0x6a09e667,
+    0xbb67ae85,
+    0x3c6ef372,
+    0xa54ff53a,
+    0x510e527f,
+    0x9b05688c,
+    0x1f83d9ab,
+    0x5be0cd19
+}
+{}
+
 void SHA256::update(const uint8_t* data, size_t length) {
     for (size_t i = 0 ; i < length ; i++) {
         m_data[m_block_length++] = data[i];
@@ -31,24 +65,24 @@ uint8_t* SHA256::digest() {
     return hash;
 }
 
-uint32_t SHA256::rotr(uint32_t x, uint32_t n) {
+uint32_t rotr(uint32_t x, uint32_t n) {
     return (x >> n) | (x << (32 - n));
 }
 
-uint32_t SHA256::choose(uint32_t e, uint32_t f, uint32_t g) {
+uint32_t choose(uint32_t e, uint32_t f, uint32_t g) {
     return (e & f) ^ (~e & g);
 }
 
-uint32_t SHA256::majority(uint32_t a, uint32_t b, uint32_t c) {
+uint32_t majority(uint32_t a, uint32_t b, uint32_t c) {
     return (a & (b | c)) | (b & c);
 }
 
-uint32_t SHA256::sig0(uint32_t x) {
-    return SHA256::rotr(x, 7) ^ SHA256::rotr(x, 18) ^ (x >> 3);
+uint32_t sig0(uint32_t x) {
+    return rotr(x, 7) ^ rotr(x, 18) ^ (x >> 3);
 }
 
-uint32_t SHA256::sig1(uint32_t x) {
-    return SHA256::rotr(x, 17) ^ SHA256::rotr(x, 19) ^ (x >> 10);
+uint32_t sig1(uint32_t x) {
+    return rotr(x, 17) ^ rotr(x, 19) ^ (x >> 10);
 }
 
 void SHA256::transform() {
@@ -63,7 +97,7 @@ void SHA256::transform() {
     }
 
     for (uint8_t k = 16 ; k < 64; k++) { // Remaining 48 blocks
-        m[k] = SHA256::sig1(m[k - 2]) + m[k - 7] + SHA256::sig0(m[k - 15]) + m[k - 16];
+        m[k] = sig1(m[k - 2]) + m[k - 7] + sig0(m[k - 15]) + m[k - 16];
     }
 
     for(uint8_t i = 0 ; i < 8 ; i++) {
@@ -71,12 +105,10 @@ void SHA256::transform() {
     }
 
     for (uint8_t i = 0; i < 64; i++) {
-        maj   = SHA256::majority(state[0], state[1], state[2]);
-        xorA  = SHA256::rotr(state[0], 2) ^ SHA256::rotr(state[0], 13) ^ SHA256::rotr(state[0], 22);
-
+        maj  = majority(state[0], state[1], state[2]);
+        xorA = rotr(state[0], 2) ^ rotr(state[0], 13) ^ rotr(state[0], 22);
         ch = choose(state[4], state[5], state[6]);
-
-        xorE  = SHA256::rotr(state[4], 6) ^ SHA256::rotr(state[4], 11) ^ SHA256::rotr(state[4], 25);
+        xorE = rotr(state[4], 6) ^ rotr(state[4], 11) ^ rotr(state[4], 25);
 
         sum  = m[i] + SHA256_KEYS[i] + state[7] + ch + xorE;
         newA = xorA + maj + sum;
@@ -136,12 +168,12 @@ void SHA256::revert(uint8_t* hash) {
     }
 }
 
-std::string SHA256::toString(const uint8_t* digest) {
+std::string SHA256::to_string(const uint8_t* digest) {
     std::stringstream s;
     s << std::setfill('0') << std::hex;
 
-    for(uint8_t i = 0 ; i < 32 ; i++) {
-        s << std::setw(2) << (unsigned int) digest[i];
+    for (uint8_t i = 0 ; i < 32 ; i++) {
+        s << std::setw(2) << (size_t) digest[i];
     }
 
     return s.str();
--- src/main.cpp
+++ src/main.cpp
@@ -12,7 +12,7 @@ int main() {
     sha.update(input);
     uint8_t* output = sha.digest();
 
-    std::cout << SHA256::toString(output) << std::endl;
+    std::cout << SHA256::to_string(output) << std::endl;
 
     delete[] output;
 }
